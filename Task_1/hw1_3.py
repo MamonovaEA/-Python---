7d21774b4119fa237c6f43c304dc5ad75f40d932
Mamonova Elena <mamonova.ea96@gmail.com>
2025-01-07 16:21:25 +0500

    Add files via upload

---
+++ Task_1/hw1_3.py
@@ -0,0 +1,15 @@
+# Создаем пустой словарь, в который затем будем добавлять записи
+task_dict = {}
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+task_dict[date] = task
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+task_dict[date] = task
+
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+task_dict[date] = task
